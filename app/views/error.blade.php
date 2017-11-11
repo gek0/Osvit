9723e89bbfc155068ed74e80579f9c25563432bd
Matija Buriša <matijaburisa@gmail.com>
2017-11-11 15:52:05 +0100

    error page
    error page design finished

--- app/views/error.blade.php
+++ app/views/error.blade.php
@@ -1,8 +1,31 @@
-@include('public.layout.header')
+@include('shared-layout.head')
 
-<div class="container content-holder">
+   <!-- scripts -->
+{{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', ['charset' => 'utf-8']) }}
+{{ HTML::script('js/bootstrap.min.js', ['charset' => 'utf-8']) }}
+{{ HTML::script('js/bootstrap-select.min.js', ['charset' => 'utf-8']) }}
+{{ HTML::script('js/modernizr-2.6.2.min.js', ['charset' => 'utf-8']) }}
+    <!--[if lt IE 9]>
+{{ HTML::script('js/html5shiv.min.js', ['charset' => 'utf-8']) }}
+{{ HTML::script('js/respond.min.js', ['charset' => 'utf-8']) }}
+<![endif]-->
+
+<!-- stylesheets -->
+{{ HTML::style('css/bootstrap.min.css') }}
+{{ HTML::style('css/main.css') }}
+{{ HTML::style('css/style.css') }}
+{{ HTML::style('css/queries.css') }}
+</head>
+<body>
+
+@include('shared-layout.browser-fb')
+
+<div class="container content-holder content-error">
+    <div class="space-x3"></div>
     <section class="logo-placeholder">
-        <h1>Dogodila se greška</h1>
+        {{ HTML::image('css/assets/images/logo_main.png', 'Logo', ['title' => getenv('WEB_NAME'), 'class' => 'img-responsive']) }}
+        <h1>Dogodila se greška<br>404</h1>
+        <h3>Stranica koju ste tražili ne postoji</h3>
     </section>
 
     <section class="section section-inner">
@@ -10,10 +33,13 @@
             <h2>{{{ $exception }}}</h2>
 
             <div class="space"></div>
-            <a href="{{ URL::route('home') }}"><button class="learn-btn-inverse animated fadeInUp">Povratak na početnu <i class="fa fa-home fa-med"></i></button></a>
+            <a href="{{ URL::route('home') }}">
+                <button class="btn btn-submit btn-submit-full">
+                    <i class="fa fa-home fa-med"></i> Povratak na početnu
+                </button>
+            </a>
         </div>
     </section> <!-- end section-inner -->
-
-    </section> <!-- end #main -->
+</div>
 
 @include('public.layout.footer')
